c68e6d1cc7c6095a20b69a5af754e6e6d5fbe656
Jorge Matos <jisapjisap@gmail.com>
2023-12-18 13:42:58 +0100

    Header

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+import Header from '@/components/Header'
 import Hero from '@/components/Hero'
 import StyleGuide from '@/components/StyleGuide'
 import Image from 'next/image'
@@ -5,8 +6,9 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className='w-full max-w-[1440px] bg-white mx-auto overflow-hidden'>
-      {/* <StyleGuide /> */}
+      <Header />
       <Hero />
+      {/* <StyleGuide /> */}
       <div className='h-[4000px]'></div>
     </main>
   )
