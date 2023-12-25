c86f8ea9060da0b4dab67a5026eecb4dd254de8a
Jorge Matos <jisapjisap@gmail.com>
2023-12-25 12:19:27 +0100

    footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -12,18 +12,28 @@ const Footer = () => {
   return (
     <footer className="bg-footer bg-cover bg-no-repeat text-white pt-16">
       <div className="container mx-auto">
-        {/* logo */}
-        <div className="w-[300px] mb-8 xl:mb-0">
-          <Link href="/">
-            <Image 
-              src="/logo.svg"
-              width={90}
-              height={36}
-              alt=""
-            />
-          </Link>
+        <div className="flex flex-col justify-between xl:flex-row">
+          {/* logo */}
+          <div className="w-[300px] mb-8 xl:mb-0">
+            <Link href="/">
+              <Image 
+                src="/logo.svg"
+                width={90}
+                height={36}
+                alt=""
+              />
+            </Link>
+          </div>
+          {/* grid items */}
+          <div className="flex-1 grid grid-cols-1 xl:grid-cols-3 gap-[50px] mb-8 xl:mb-16">
+            {/* blog */}
+            <div>Blog</div>
+            {/* item */}
+            <div>item</div>
+            {/* socials */}
+            <div>Socials</div>
+          </div>
         </div>
-        {/* grid items */}
       </div>
     </footer>
   )
