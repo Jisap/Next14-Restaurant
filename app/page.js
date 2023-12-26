1a727f9e367aa9ea390bbb1abfbd683b480b6098
Jorge Matos <jisapjisap@gmail.com>
2023-12-26 16:26:36 +0100

    finish

--- app/page.js
+++ app/page.js
@@ -5,8 +5,8 @@ import Hero from '@/components/Hero'
 import Map from '@/components/Map'
 import Menu from '@/components/Menu'
 import Reservation from '@/components/Reservation'
-import StyleGuide from '@/components/StyleGuide'
-import Image from 'next/image'
+//import StyleGuide from '@/components/StyleGuide'
+
 
 export default function Home() {
   return (
